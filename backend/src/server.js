98ac2198947922199889b982dfd842863c74d530
Divya Sri <119841245+divyasrisaipravallika@users.noreply.github.com>
2026-01-03 16:57:37 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
 const app = require('./app');
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
@@ -10,4 +9,5 @@ const bcrypt = require('bcrypt');
 
 bcrypt.hash('Admin@123', 10).then(console.log);
 bcrypt.hash('Demo@123', 10).then(console.log);
+
 

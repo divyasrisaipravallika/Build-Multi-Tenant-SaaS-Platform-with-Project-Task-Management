b505e3cb3d9297347fc6a25ab8ae261d2507f9f0
Divya Sri <119841245+divyasrisaipravallika@users.noreply.github.com>
2026-01-03 16:56:24 +0530

    Update task.routes.js

--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -8,9 +8,9 @@ router.post('/projects/:projectId/tasks', auth, taskController.createTask);
 router.get('/projects/:projectId/tasks', auth, taskController.listTasks);
 
 router.get('/tasks/all', auth, taskController.listAllTasks);
-
 router.patch('/tasks/:taskId/status', auth, taskController.updateTaskStatus);
 router.put('/tasks/:taskId', auth, taskController.updateTask);
 router.delete('/tasks/:taskId', auth, taskController.deleteTask);
 
 module.exports = router;
+

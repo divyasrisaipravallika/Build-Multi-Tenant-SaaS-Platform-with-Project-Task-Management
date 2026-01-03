eb7b4727542c5233ff31f61b90e9d6d2424699f9
Divya Sri <119841245+divyasrisaipravallika@users.noreply.github.com>
2026-01-03 16:55:39 +0530

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth.middleware');
 const controller = require('../controllers/auth.controller');
-
 router.post('/register-tenant', controller.registerTenant);
 router.post('/login', controller.login);
 router.get('/me', auth, controller.me);
 router.post('/logout', auth, controller.logout);
 
 module.exports = router;
+
